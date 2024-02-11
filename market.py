c761fb86da82f131bafbc5b4d1eb0216ce2bd2cd
vvkovalchuk <vvkovalchuk0914@gmail.com>
2024-02-11 18:50:47 +0200

    Styling & Templates

--- market.py
+++ market.py
@@ -1,11 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World</h1>'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username: str):
-    return f'<h1>This is about page of {username}</h1>'
 
